3e219f74ee44e7430c82011b73f0020b3e8c7720
노휘겸 <whistle96@gmail.com>
2015-11-18 08:57:57 +0900

    자동 구현 속성 향상 추가

---
+++ CSharp6/ImprovementAutoProperty.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp6
+{
+    class ImprovementAutoProperty
+    {
+        public string Key { get; } = Guid.NewGuid().ToString("N");
+        public string Subject { get; set; } = "TODO:";
+        public bool IsComplete { get; set; }
+        public DateTime? Updated { get; private set; } = DateTime.Now;
+    }
+}
