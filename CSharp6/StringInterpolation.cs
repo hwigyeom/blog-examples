5779bbc69e8fa0f7284b8701d42c24bc4d8a4173
노휘겸 <whistle96@gmail.com>
2015-11-17 22:55:28 +0900

    조건식 추가

--- CSharp6/StringInterpolation.cs
+++ CSharp6/StringInterpolation.cs
@@ -12,10 +12,11 @@ namespace CSharp6
         {
             string siteName = "블로그";
             string userName = "방문자";
+            bool isFirstVisit = true;
             int visitorCount = 20;
             float ratio = 0.3F;
 
-            string formatResult = string.Format("{0}님 {1}에 오신것을 환영합니다.", userName, siteName);
+            string formatResult = string.Format("{0}님 {1}에 오신것을 환영합니다.{2}", userName, siteName, isFirstVisit ? "처음 방문해 주셨네요": "");
             string concatResult = userName + "님 " + siteName + "에 오신것을 환영합니다.";
 
             string visitorMessage = string.Format("{0,5}분이 현재 접속 중이며, 현재 점수 가중치는 {1,5:F2}입니다.", visitorCount, ratio);
@@ -27,10 +28,11 @@ namespace CSharp6
         {
             string siteName = "블로그";
             string userName = "방문자";
+            bool isFirstVisit = true;
             int visitorCount = 20;
             float ratio = 0.3F;
 
-            string result = $"{userName}님 {siteName}에 오신것을 환영합니다.";
+            string result = $"{userName}님 {siteName}에 오신것을 환영합니다.{(isFirstVisit ? " 처음 방문해 주셨네요" : "")}";
             string visitorMessage = $"{visitorCount,5}분이 현재 접속 중이며, 현재 점수 가중치는 {ratio,5:F2}입니다.";
 
             Console.WriteLine(visitorMessage);
