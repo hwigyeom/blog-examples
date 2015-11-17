0fbccca2a1a6656865c9ca94a9ea27572cf25a69
노휘겸 <whistle96@gmail.com>
2015-11-17 17:49:51 +0900

    nameof 연산자 코드 추가

---
+++ CSharp6/NameofOperator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp6
+{
+    public static class NameofOperator
+    {
+        public static string Append(this string str, string appendString)
+        {
+            if (str == null)
+            {
+                throw new ArgumentNullException(nameof(str));
+            }
+            if (appendString == null)
+            {
+                throw new ArgumentNullException(nameof(appendString));
+            }
+
+            str = string.Concat(str, appendString);
+
+            return str;
+        }
+    }
+}
