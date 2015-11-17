b29204422f283207443e318e655ab6882bae1d5d
노휘겸 <whistle96@gmail.com>
2015-11-17 23:00:24 +0900

    결과 추가

--- CSharp6/StringInterpolation.cs
+++ CSharp6/StringInterpolation.cs
@@ -16,12 +16,16 @@ namespace CSharp6
             int visitorCount = 20;
             float ratio = 0.3F;
 
-            string formatResult = string.Format("{0}님 {1}에 오신것을 환영합니다.{2}", userName, siteName, isFirstVisit ? "처음 방문해 주셨네요": "");
-            string concatResult = userName + "님 " + siteName + "에 오신것을 환영합니다.";
-
+            string formatResult = string.Format("{0}님 {1}에 오신것을 환영합니다.{2}", userName, siteName, isFirstVisit ? " 처음 방문해 주셨네요": "");
+            string concatResult = userName + "님 " + siteName + "에 오신것을 환영합니다." + (isFirstVisit ? " 처음 방문해 주셨네요" : "");
             string visitorMessage = string.Format("{0,5}분이 현재 접속 중이며, 현재 점수 가중치는 {1,5:F2}입니다.", visitorCount, ratio);
 
+            Console.WriteLine(formatResult);
+            Console.WriteLine(concatResult);
             Console.WriteLine(visitorMessage);
+            // 방문자님 블로그에 오신것을 환영합니다. 처음 방문해 주셨네요
+            // 방문자님 블로그에 오신것을 환영합니다. 처음 방문해 주셨네요
+            //    20분이 현재 접속 중이며, 현재 점수 가중치는  0.30입니다.
         }
 
         public void Interpolation()
@@ -35,7 +39,11 @@ namespace CSharp6
             string result = $"{userName}님 {siteName}에 오신것을 환영합니다.{(isFirstVisit ? " 처음 방문해 주셨네요" : "")}";
             string visitorMessage = $"{visitorCount,5}분이 현재 접속 중이며, 현재 점수 가중치는 {ratio,5:F2}입니다.";
 
+            Console.WriteLine(result);
             Console.WriteLine(visitorMessage);
+
+            // 방문자님 블로그에 오신것을 환영합니다. 처음 방문해 주셨네요
+            //    20분이 현재 접속 중이며, 현재 점수 가중치는  0.30입니다.
         }
     }
 }
